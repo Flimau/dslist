7c8abf30e419b84f1d6d4962b9e3067e4b324466
flimau <fer.ulrich@gmail.com>
2025-01-25 18:42:35 -0300

    Game by id

--- src/main/java/com/flimau/dslist/controllers/GameController.java
+++ src/main/java/com/flimau/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.flimau.dslist.controllers;
 
+import com.flimau.dslist.dto.GameDTO;
 import com.flimau.dslist.dto.GameMinDTO;
 import com.flimau.dslist.entities.Game;
 import com.flimau.dslist.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +23,11 @@ public class GameController {
     public List<GameMinDTO> findAll(){
         return service.findAll();
     }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+
 }
--- src/main/java/com/flimau/dslist/service/GameService.java
+++ src/main/java/com/flimau/dslist/service/GameService.java
@@ -1,21 +1,41 @@
 package com.flimau.dslist.service;
 
+import com.flimau.dslist.dto.GameDTO;
 import com.flimau.dslist.dto.GameMinDTO;
 import com.flimau.dslist.entities.Game;
 import com.flimau.dslist.repository.GameRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
-@Service//registrar componentes para que o framework o gerencie, para isso a anotação service, ou pode usar a component
+//registrar componentes para que o framework o gerencie, para isso a anotação service, ou pode usar a component
+@Service
 public class GameService{
 
     @Autowired// injeção do componente repository
     private GameRepository repository;
 
+    /*
+    * A anotação @Transactional do Spring é usada para gerenciar transações de banco de dados.
+    * Ela garante que todas as operações dentro de um método anotado sejam executadas em uma única transação.
+    * Se algo der errado, as alterações no banco são revertidas (rollback).
+    * Se tudo ocorrer bem, elas são confirmadas (commit).
+    * A propriedade readOnly = true é usada para indicar que o método apenas lê dados do banco de dados
+    *  e não realiza nenhuma alteração.
+    */
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = repository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = repository.findById(id).get();
+        return new GameDTO(result);
     }
 }
